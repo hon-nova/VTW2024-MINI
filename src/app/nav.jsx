2e3e4f54a5aab6aeaa31c098543d1a980e2cf16d
hon-nova <hon.tnguyen294@gmail.com>
2024-09-14 12:07:20 -0700

    added nav and footer

---
+++ src/app/nav.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="flex-row">
+      <h1>VIELE</h1>
+      <button>Login</button>
+    </div>
+  )
+}
